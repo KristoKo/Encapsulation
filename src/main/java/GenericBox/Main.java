b257cc015255c547f18e87be187f65c40cba7080
KristoK <kristokonsap@gmail.com>
2020-06-21 12:25:21 +0300

    a

--- src/main/java/GenericBox/Main.java
+++
@@ -1,9 +0,0 @@
-package GenericBox;
-
-public class Main {
-    public static void main(String[] args) {
-        Vehicle vehicle = new Vehicle(4, "ABC-123");
-        
-        GenericBox<Vehicle> genericBoxer = new GenericBox<>(vehicle);
-    }
-}
